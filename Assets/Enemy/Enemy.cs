07870402d3cb4b390ed82477f89336b77c70baec
nuevakenia <d.galli@alumnos.duoc.cl>
2021-02-03 14:38:05 -0300

    0.1
    Add: Hp.
    Add: Enemigo:
    Add: Pickups:
    Add: UI.
    Add: Doble salto.

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using TMPro;
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float hpPlayer;
+    public int damage;
+    // patruyar
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Atacar
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //Estados
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    // HP
+    public Text txtHp;
+    public Text txtWin;
+    private void Awake()
+    {
+        player = GameObject.Find("Jugador").transform;
+        agent = GetComponent<NavMeshAgent>();
+        damage = 15;
+        hpPlayer = GameObject.Find("Jugador").GetComponent<PlayerController>().hpPlayer;
+        UpdateHp();
+    }
+
+    void Start()
+    {
+        //txtWin.text = "aerr";
+        Debug.Log("Iniciando void start: ");
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Attack code here
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 4f, ForceMode.Impulse);
+
+            alreadyAttacked = true;
+
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+
+    public void TakeDamageEnemy(int damage)
+    {
+        hpPlayer -= damage;
+        UpdateHp();
+        if (hpPlayer <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    void UpdateHp()
+    {
+        txtHp.text = "HP: " + hpPlayer.ToString();
+        if (hpPlayer <= 0)
+        {
+            //
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad;
+    public Text txtContador;
+    public Text winText;
+    public Text txtHp;
+
+    private Rigidbody rb;
+    private int contador;
+
+    // Salto
+
+    private float jumpSpeed = 5;
+    private bool onGround = true;
+    private const int maxJump = 2;
+    public int currentJump = 0;
+    public float hpPlayer;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+       // hp = GameObject.Find("Enemigo").GetComponent<Enemy>().health;
+        contador = 0;
+        UpdatePuntaje();
+        //winText.text = "";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space") && (onGround || maxJump > currentJump))
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            onGround = false;
+            currentJump++;
+        }
+    }
+
+  
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * velocidad);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            contador = contador + 1;
+            UpdatePuntaje();
+        }
+       
+    }
+
+    void UpdatePuntaje()
+    {
+        txtContador.text = "Puntaje: " + contador.ToString();
+        if (contador >= 9)
+        {
+            winText.text = "Ganaste!";
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name);
+        onGround = true;
+        currentJump = 0;
+
+
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            hpPlayer -= 25;
+           // Destroy(collision.gameObject);
+            Debug.Log("Me pegooo");
+            UpdateHp();
+        }
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+      //  hpPlayer -= damage;
+       // UpdateHp();
+    //    if (hpPlayer <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+
+    void UpdateHp()
+    {
+        txtHp.text = "HP: " + hpPlayer.ToString();
+        if (hpPlayer <= 0)
+        {
+            //
+            Debug.Log("Estas muerto!");
+            winText.text = "ESTAS MUERTO";
+        }
+    }
+}
